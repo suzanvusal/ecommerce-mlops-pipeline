8638083bcdd0d857742826c61cca614d867d05c2
suzanvusal <86911143+suzanvusal@users.noreply.github.com>
2026-05-18 20:26:17 +0000

    feat: implement holiday effect modelling for Black Friday etc

---
+++ src/forecasting/forecast_store.py
@@ -0,0 +1,9 @@
+"""
+src/forecasting/forecast_store.py
+Day 18: Demand forecasting with Prophet
+Focus: Facebook Prophet for time-series demand forecasting, seasonality
+"""
+from __future__ import annotations
+import logging
+
+logger = logging.getLogger(__name__)
