7bc65e8f2423efe9dcde65c40b8612d76e98a8d8
suzanvusal <86911143+suzanvusal@users.noreply.github.com>
2026-07-21 20:19:15 +0000

    chore: add logging to test_als_model

--- tests/unit/test_als_model.py
+++ tests/unit/test_als_model.py
@@ -29,3 +29,5 @@ logger = logging.getLogger(__name__)
 # 20:47:50 — chore: add logging to test_als_model
 
 # 20:16:02 — refactor: extract constant in test_als_model
+
+# 20:19:15 — chore: add logging to test_als_model
